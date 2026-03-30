9b182d334d96bc872c2a776777aa3b1a9ff409a0
KINGIRIS1 <91662273+KINGIRIS1@users.noreply.github.com>
2026-03-30 14:01:39 +0700

    feat: Update Supabase credentials
    Replaced Supabase URL and anon public key with new values. This change is necessary to connect to the correct Supabase project for application data management.

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 // Bạn cần thay thế 2 giá trị dưới đây bằng thông tin từ Project Supabase của bạn.
 // Vào: Project Settings -> API -> Lấy URL và "anon public" Key.
 
-const SUPABASE_URL = 'https://dajjhubrhybodggbqapt.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhampodWJyaHlib2RnZ2JxYXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NzM3MDUsImV4cCI6MjA4MDM0OTcwNX0.Te4JGaR7DnSiejugyZHV0_uQSWsG_TS_xTmRgxgM5-4';
+const SUPABASE_URL = 'https://ogrronmwdpryokbyoeda.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ncnJvbm13ZHByeW9rYnlvZWRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NDgwMjMsImV4cCI6MjA5MDQyNDAyM30.D91hzb7jEtrN1l72qTB1H4Cm63ImiRQDX_ffI5IVw3o';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
